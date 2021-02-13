acc596d2d1d1b60ce28f3c4a5061c72caf749f9a
Valeria Rodionova <valeriya_off@mail.ru>
2021-02-14 00:26:31 +0300

    Add URI mongo claster

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.use(express.json());
+
+const database = require('./config/keys').mongoURI;
+
+mongoose
+  .connect(database)
+  .then(() => console.log('connect...'))
+  .catch(err => console.log(err));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`started on port ${port}`));
