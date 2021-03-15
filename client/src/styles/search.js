8ee1f0238b61a2fc2c793e7a9f713efa3a0960fa
Valeria Rodionova <valeriya-off@yandex.ru>
2021-03-16 01:49:36 +0300

    Feat: add styles of search

--- client/src/styles/search.js
+++ client/src/styles/search.js
@@ -3,9 +3,15 @@ import styled from 'styled-components/macro';
 const Main = styled.main`
   display: flex;
   justify-content: center;
-  align-items: center;
+  align-items: center; 
   height: 100vh;
   background-color: #FAFAFA;
+
+  &.active {
+    justify-content: flex-start;
+    align-items: flex-start;
+    padding: 120px 0 0 0;
+  }
 `
 
 const SearchContainer = styled.div`
@@ -13,13 +19,25 @@ const SearchContainer = styled.div`
   flex-direction: column;
   align-items: center;
   padding: 0 20px;
-  max-width: 1080px;
+  max-width: 1060px;
+  
+  &.active {
+    align-items: flex-start;
+    margin: 0 auto;
+    width: 100%
+  }
 `
 
 const SearchTitle = styled.h1`
   margin: 0 0 40px 0;
   font-size: 36px;
   line-height: 52px;
+
+  &.active {
+    margin: 0 0 10px 0;
+    font-size: 28px;
+    line-height:40px;
+  }
 `
 
 const SearchInner = styled.div`
@@ -33,6 +51,10 @@ const SearchInput = styled.input`
   font-size: 20px;
   border: 1px solid #D1D1D1;
   border-radius: 5px 0 0 5px;
+
+  &.active {
+    width: 870px;
+  }
 
   ::placeholder {
     font-size: 20px;
@@ -50,12 +72,18 @@ const SearchInput = styled.input`
 
 const SearchBtn = styled.button`
   padding: 14px 35px;
+  display: flex;
+  justify-content: center;
   max-width: 150px;
   color: #fff;
   font-size: 20px;
   background-color: #1390E5;
   border: 1px solid #1390E5;
   border-radius: 0 5px 5px 0;
+
+  &.active {
+    padding: 14px 50px;
+  }
 `
 
 export { Main, SearchContainer, SearchTitle, SearchInner, SearchInput, SearchBtn }
