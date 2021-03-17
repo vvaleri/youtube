253556192c7eae9a53eb9efa582e81dfae91d9a7
Valeria Rodionova <valeriya-off@yandex.ru>
2021-03-18 01:07:10 +0300

    Feat: request youtube API

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -1,14 +1,31 @@
+import { useState } from 'react';
 import { Results } from '../components';
 import { Main, SearchContainer, SearchTitle, SearchInner, SearchInput, SearchBtn } from '../styles/search';
+import apiKey  from '../config/key';
 
 export function Search() {
+
+  const [ video, setVideo ] = useState([]);
+  const [ inputValue, setInputValue ] = useState('');
+  
+  const getVideo = async () => {
+    const response = await fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=${ inputValue }&key=${ apiKey }`);
+    const result = await response.json();
+    setVideo(result)
+  }
+
   return (
     <Main>
       <SearchContainer>
         <SearchTitle>Поиск видео</SearchTitle>
         <SearchInner>
-          <SearchInput placeholder='Что хотите посмотреть?'/>
-          <SearchBtn>Найти</SearchBtn>
+          <SearchInput 
+            type='text'
+            placeholder='Что хотите посмотреть?'
+            value={ inputValue }
+            onChange={ (e) => setInputValue(e.target.value) }
+          />
+          <SearchBtn onClick={ getVideo }>Найти</SearchBtn>
         </SearchInner>
       </SearchContainer>
       <Results/>
