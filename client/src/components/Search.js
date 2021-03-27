a6834e66b7cd4796893aefef8e2cce3c07224434
Valeria Rodionova <valeriya-off@yandex.ru>
2021-03-28 01:35:26 +0300

    Feat: create custom hook

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -1,34 +1,23 @@
-import { useState } from 'react';
-import { Results } from '.';
+import { Results } from '../components';
 import { Main, SearchContainer, SearchTitle, SearchInner, SearchInput, SearchBtn } from '../styles/search';
-import apiKey  from '../config/key';
+import useFetch from '../hooks/useFetch';
 
 export function Search() {
 
-  const [ video, setVideo ] = useState([]);
-  const [ inputValue, setInputValue ] = useState('');
-  const [ activeClass, setActiveClass ] = useState(false);
-  
-  const getVideo = async () => {
-    const response = await fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=${ inputValue }&key=${ apiKey }`);
-    const result = await response.json();
-    setVideo(result)
-    setActiveClass(true);
-  }
+  const { inputValue, setInputValue, getVideo } = useFetch();
 
   return (
-    <Main className={ activeClass ? 'active' : '' }>
-      <SearchContainer className={ activeClass ? 'active' : '' }>
-        <SearchTitle className={ activeClass ? 'active' : '' }>Поиск видео</SearchTitle>
+    <Main>
+      <SearchContainer>
+        <SearchTitle>Поиск видео</SearchTitle>
         <SearchInner>
           <SearchInput 
             type='text'
             placeholder='Что хотите посмотреть?'
-            className={ activeClass ? 'active' : '' }
             value={ inputValue }
             onChange={ (e) => setInputValue(e.target.value) }
           /> 
-          <SearchBtn className={ activeClass ? 'active' : '' } onClick={ getVideo }>Найти</SearchBtn>
+          <SearchBtn onClick={ getVideo }>Найти</SearchBtn>
         </SearchInner>
       </SearchContainer>
       <Results/>
---
+++ client/src/hooks/useFetch.js
@@ -0,0 +1,17 @@
+import { useState } from 'react';
+import apiKey  from '../config/key';
+
+export default function useFetch() {
+  const [ video, setVideo ] = useState('');
+  const [ inputValue, setInputValue ] = useState('');
+  const [ mainSearch, setMainSearch ] = useState(false);
+
+  const getVideo = async () => {
+    const response = await fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=${ inputValue }&key=${ apiKey }`);
+    const result = await response.json();
+    setVideo(result)
+    setMainSearch(true);
+  }
+
+  return { mainSearch, inputValue, setInputValue, getVideo }
+}
