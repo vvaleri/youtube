55b2233153f9a6197b31b33729c4b959310dc60e
Valeria Rodionova <valeriya-off@yandex.ru>
2021-03-29 23:41:49 +0300

    Feat: save state in localstorage

--- client/src/components/IntroSearch.js
+++ client/src/components/IntroSearch.js
@@ -1,10 +1,9 @@
 import { Search } from '../components';
 import { Main, IntroContainer, IntroTitle, IntroInner, IntroInput, IntroBtn } from '../styles/introSearch';
 import useFetch from '../hooks/useFetch';
-import { useEffect } from 'react';
 
 export function IntroSearch() {
-    const { mainSearch, inputValue, setInputValue, getVideo } = useFetch();
+    const { mainSearch, inputValue, setInputValue, getComponent } = useFetch();
 
     return mainSearch ? (<Search/>) : (
       <Main>
@@ -17,7 +16,7 @@ export function IntroSearch() {
               value={ inputValue }
               onChange={ (e) => setInputValue(e.target.value) }
             />
-            <IntroBtn onClick={ getVideo }>Найти</IntroBtn>
+            <IntroBtn onClick={ getComponent }>Найти</IntroBtn>
           </IntroInner>
         </IntroContainer>
       </Main>
--- client/src/hooks/useFetch.js
+++ client/src/hooks/useFetch.js
@@ -1,8 +1,8 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import apiKey  from '../config/key';
 
 export default function useFetch() {
-  const [ video, setVideo ] = useState('');
+  const [ video, setVideo ] = useState([]);
   const [ inputValue, setInputValue ] = useState('');
   const [ mainSearch, setMainSearch ] = useState(false);
 
@@ -10,8 +10,19 @@ export default function useFetch() {
     const response = await fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=${ inputValue }&key=${ apiKey }`);
     const result = await response.json();
     setVideo(result)
-    setMainSearch(true);
   }
 
-  return { mainSearch, setMainSearch, inputValue, setInputValue, getVideo }
+  const getComponent = () => {
+    getVideo()
+    setMainSearch(true);
+    localStorage.setItem('mainSearch', 'true')
+  }
+
+  useEffect(() => {
+    if (localStorage.getItem('mainSearch')) {
+      setMainSearch(true);
+    } 
+  }, [])
+
+  return { mainSearch, setMainSearch, inputValue, setInputValue, getVideo, getComponent }
 }
